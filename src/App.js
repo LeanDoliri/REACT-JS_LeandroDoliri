02238c803e97ef9def64ddd9d9c8c8bec850de6e
Leandro Doliri <leandoliri@gmail.com>
2022-01-17 13:46:15 -0300

    subida 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavbarYendo from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+        <NavbarYendo />
+    </div>
+  );
+}
+
+export default App;
